d68e7005cacc0d01f664585aa3e035d0a89c589c
unknown <ranjithm1193@gmail.com>
2021-06-25 17:37:52 +0530

    [Ranjith] added TwiceNumber.js

---
+++ TwiceNumber.js
@@ -0,0 +1,12 @@
+function printTwiceNumber( )
+{
+    var i , n = 8,targetNum=11;
+    console.log(targetNum);
+    for (i = 1 ; i<=n; i++) 
+        {
+            targetNum= targetNum+11;
+            console.log(targetNum);
+        }    
+        console.log(100);   
+}
+printTwiceNumber();
